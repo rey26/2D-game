97f60db2e26255cb0a491e6b78de0f22e085d2e5
mm759me <matej.malicky@student.tuke.sk>
2019-12-04 07:59:13 +0100

    10th lab, add map

--- src/main/java/sk/tuke/kpi/oop/game/Main.java
+++ src/main/java/sk/tuke/kpi/oop/game/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
         // vytvorenie sceny pre hru
         // pouzijeme implementaciu rozhrania `Scene` triedou `World`
-        Scene scene = new World("world");
+        Scene scene = new World("world", "maps/mission-impossible.tmx");
         // pridanie sceny do hry
         game.addScene(scene);
 
